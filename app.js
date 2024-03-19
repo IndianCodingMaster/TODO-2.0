f272df6bf689bc9bece704245bb0d42253031ab5
Satya Prakash <satyaprakash91180@gmail.com>
2024-03-19 23:52:53 +0530

    .

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const todoList = document.getElementById("todoList");
 const todoCount = document.getElementById("todoCount");
 const addButton = document.querySelector(".add-btn");
 const deleteAllButton = document.querySelector(".clear");
-
 addButton.addEventListener("click", addTask);
 deleteAllButton.addEventListener("click", deleteAllTasks);
 
@@ -12,8 +11,9 @@ displayTasks();
 
 function addTask() {
   const newTask = todoInput.value.trim();
+
   if (newTask !== "") {
-    todo.push({ text: newTask });
+    todo.push({ text: newTask, completed: false });
     saveToLocalStorage();
     todoInput.value = "";
     displayTasks();
@@ -26,12 +26,27 @@ function displayTasks() {
     const li = document.createElement("li");
     li.innerHTML = `
                   <div>
-                    <span><input type="checkbox" /></span>
+                    <span class="check"><input class="checkbox" type="checkbox" /></span>
                     <span>${item.text}</span>
                   </div>
                   <button class="deleteBtn"><i class="fa-solid fa-xmark"></i></button>
             `;
     todoList.appendChild(li);
+
+    const checkbox = li.querySelector(".checkbox");
+    const check = li.querySelector(".check");
+    checkbox.addEventListener("change", () => {
+      item.completed = !item.completed;
+      saveToLocalStorage();
+      displayTasks();
+    });
+
+    if (item.completed) {
+      li.classList.add("completed");
+      check.innerHTML = '<i class="fa-solid fa-check"></i>';
+    } else {
+      li.classList.remove("completed");
+    }
   });
 
   todoCount.textContent = todo.length;
